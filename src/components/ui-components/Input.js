e2f87348ab72a93834902673f983a8b0cda75ab7
LukeJessop <jessop.lucas@gmail.com>
2022-10-19 17:14:08 -0600

    got all styling aspcets up to where I want it to be, configured the inputs so they turn red at the right times to show validity

--- src/components/ui-components/Input.js
+++ src/components/ui-components/Input.js
@@ -4,9 +4,16 @@ import { PatternFormat } from "react-number-format";
 
 function Input(props) {
   const [valid, setValid] = useState(true);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const [uCase, setUCase] = useState(false);
+  const [lCase, setLCase] = useState(false);
+  const [specialChar, setSpecialChar] = useState(false);
+  const [num, setNum] = useState(false);
+  const [validLength, setValidLength] = useState(false);
+  const [validPassword, setValidPassword] = useState();
 
   useEffect(() => {
-    setValid(true);
     if (props.isSubmitted) {
       if (!props.value) {
         setValid(false);
@@ -16,21 +23,50 @@ function Input(props) {
           setValid(false);
         }
       }
-      if (props.children === "Password" || props.children === "Confirm Password") {
-        if (!props.doPasswordMatch || props.value === "") {
-          setValid(false);
+      if (props.page === 'Register' && props.children === "Password" || props.children === "Confirm Password") {
+        if (props.doPasswordMatch && props.value !== "") {
+          if(props.children === 'Password' && validPassword){
+            setValid(true)
+          }else if(props.children !=='Password'){
+            setValid(true)
+          }else{
+            setValid(false);
+          }
         } else {
+          setValid(false);
+        }
+      }
+      if (props.children === "Email") {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(props.value)) {
           setValid(true);
+        }else{
+          setValid(false);
         }
       }
     }
   }, [props.isSubmitted, props.doPasswordMatch]);
 
-  const formatPhoneNumber = (text) => {};
+  useEffect(() => {
+    if (props.page === 'Register' && uCase && lCase && specialChar && num && validLength) {
+      setValidPassword(true);
+    } else {
+      setValidPassword(false);
+    }
+  }, [uCase, lCase, specialChar, num, validLength]);
+
+  const passwordAcceptance = (password) => {
+    password.toLowerCase() != password ? setUCase(true) : setUCase(false);
+    password.toUpperCase() != password ? setLCase(true) : setLCase(false);
+    password.length > 8 ? setValidLength(true) : setValidLength(false);
+    password.search(/[^\w\ ]/g) >= 0
+      ? setSpecialChar(true)
+      : setSpecialChar(false);
+    password.search(/[\d]/g) >= 0 ? setNum(true) : setNum(false);
+    
+  };
 
   return (
     <div className={styles.container}>
-      <p>{props.children}:</p>
       {props.children === "Phone Number" ? (
         <div className={styles.phone}>
           <select className={styles.dropdown}>
@@ -45,7 +81,7 @@ function Input(props) {
             className={
               valid ? styles.input__phone : styles.input__invalid__phone
             }
-            onValueChange={(e) => props.setStateValue(e.formattedValue)}
+            onValueChange={(e) => props.setStateValue("+1" + e.value)}
             onClick={() => {
               setValid(true);
               props.setSubmitted(false);
@@ -55,21 +91,51 @@ function Input(props) {
           ></PatternFormat>
         </div>
       ) : (
-        <input
-          type={
-            props.children === "Password" ||
-            props.children === "Confirm Password"
-              ? "password"
-              : "text"
-          }
-          placeholder={props.children}
-          className={valid ? styles.input : styles.input__invalid}
-          onChange={(e) => props.setStateValue(e.target.value)}
-          onClick={() => {
-            setValid(true);
-            props.setSubmitted(false);
-          }}
-        ></input>
+        <>
+          <input
+            type={
+              props.children === "Password" ||
+              props.children === "Confirm Password"
+                ? "password"
+                : "text"
+            }
+            placeholder={
+              props.children === "Email" ? "example@gmail.com" : props.children
+            }
+            className={valid ? styles.input : styles.input__invalid}
+            onChange={(e) => {
+              props.setStateValue(e.target.value);
+              if (props.page === "Register" && props.children === "Password") {
+                setIsTyping(true);
+                passwordAcceptance(e.target.value);
+              }
+            }}
+            onClick={() => {
+              setValid(true);
+              props.setSubmitted(false);
+            }}
+          ></input>
+          {isTyping && (
+            <>
+              <h5>Your password must meet criteria:</h5>
+              <ul>
+                <li className={uCase && styles.accepted}>
+                  One upper case letter
+                </li>
+                <li className={lCase && styles.accepted}>
+                  One lower case letter
+                </li>
+                <li className={specialChar && styles.accepted}>
+                  One non-alphanumeric character
+                </li>
+                <li className={num && styles.accepted}>One number</li>
+                <li className={validLength && styles.accepted}>
+                  Atleast 8 characters long
+                </li>
+              </ul>
+            </>
+          )}
+        </>
       )}
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {BrowserRouter, HashRouter} from 'react-router-dom';
-const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
+import Img1 from "./SCSS/assets/background-images/1.jpg";
+import Img2 from "./SCSS/assets/background-images/2.jpg";
+import Img3 from "./SCSS/assets/background-images/3.jpg";
+import Img4 from "./SCSS/assets/background-images/4.jpg";
+import Img6 from "./SCSS/assets/background-images/6.jpg";
+import Img7 from "./SCSS/assets/background-images/7.jpg";
+import Img8 from "./SCSS/assets/background-images/8.jpg";
+import Img9 from "./SCSS/assets/background-images/9.jpg";
+import Img10 from "./SCSS/assets/background-images/10.jpg";
+import Img11 from "./SCSS/assets/background-images/11.jpg";
+import Img12 from "./SCSS/assets/background-images/12.jpg";
+import Img13 from "./SCSS/assets/background-images/13.jpg";
+
+const imgArr = [
+  Img1,
+  Img2,
+  Img3,
+  Img4,
+  Img6,
+  Img7,
+  Img8,
+  Img9,
+  Img10,
+  Img11,
+  Img12,
+  Img13
+];
+const generateImg = () => {
+  let rand = Math.floor(Math.random() * imgArr.length);
+  for (let i = 0; i < imgArr.length; i++) {
+    if (rand === i) {
+      return imgArr[i];
+    }
+  }
+};
+
+const Router =
+  process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+        <img className="background_img" src={generateImg()}/>
+        <App />
     </Router>
   </React.StrictMode>
 );
